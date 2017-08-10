8bbbf90664276cf7844fcb834c24326999ffd86b
Ana <ana.toma92@gmail.com>
2017-08-10 12:23:51 +0300

    add homepage

---
+++ README.md
@@ -0,0 +1,32 @@
+# Request Header Parser Microservice
+
+- - -
+
+### User Stories
+
+1. I can get the IP address, language and operating system for my browser.
+
+- - -
+
+### Example usage
+
+[URL]/api
+
+### Example output
+
+```{
+"ipaddress":"XXX.XXX.XXX.XXX",
+"language":"en-EN",
+"software":"Windows NT 10.0; Win64; x64"
+}```
+
+### Technologies used
+
+- HTML
+- CSS
+- Node.js
+- Express.js
+
+- - -
+
+*This is a **FreeCodeCamp** assignment.*
--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// from boilerplate !!!
+
+if (!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next) {
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    var origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
     next();
   });
+}
 
-app.get('/', function(req,res){
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.route('/_api/package.json')
+  .get(function(req, res, next) {
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if(err) return next(err);
+      res.type('txt').send(data.toString());
+    });
+  });
+  
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    })
+/// end of boilerplate
+
+app.get('/api', function(req,res){
 
     res.json({
         "ipaddress": req.connection.remoteAddress,
